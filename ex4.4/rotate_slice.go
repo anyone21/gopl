f77aae4349ab094de64675e4894e54cf1674ad7b
annoying_code <32765126+anyone21@users.noreply.github.com>
2021-07-06 18:33:14 +0530

    Update rotate_slice.go
    New rotate function is added

--- ex4.4/rotate_slice.go
+++ ex4.4/rotate_slice.go
@@ -11,6 +11,17 @@ func rotate_ints(ints []int) {
 	ints[len(ints)-1] = first
 }
 
+// space is O(n)
+// This function left rotate an user inut number of shifts with O(n) space complexity
+func rotate(s []int,shift int)[]int{  
+	var newdata []int
+	shift = shift%len(s)
+	first := s[:shift]
+	newdata = append(newdata,s[shift:]...)
+	newdata = append(newdata,first...)
+	return newdata
+}
+
 func main() {
 	s := []int{1, 2, 3, 4, 5}
 	rotate_ints(s)
